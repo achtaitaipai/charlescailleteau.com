1e058a2947c7ecb578a74c61c2aff0fdbbc2af01
achtaitaipai <charles.cailleteau@gmail.com>
2024-01-15 20:35:21 +0100

    improve performances

--- src/routes/homeComponents/ProjectCard/_Iframe.svelte
+++ src/routes/homeComponents/ProjectCard/_Iframe.svelte
@@ -10,6 +10,9 @@
 	$: minWidth = wrapperWidth > 0 ? Math.ceil((320 / wrapperWidth) * 100) : 50;
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href={src} />
+</svelte:head>
 <div class="container">
 	<div class="iframeWrapper">
 		<iframe {src} {title} frameborder="0" style="--_width:{iframeWidth}"></iframe>
