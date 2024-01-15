f1f2fd120314b0c9ddaf433bdfe195eb8f04160b
achtaitaipai <charles.cailleteau@gmail.com>
2024-01-15 10:22:18 +0100

    seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../index.css';
 </script>
 
+<svelte:head>
+	<title>Charles Cailleteau</title>
+	<meta name="description" content="Charles Cailleteau est designer et développeur web." />
+</svelte:head>
+
 <slot />
