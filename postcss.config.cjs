4d44dc5c0cd38b0e731211b27a196746f411809f
achtaitaipai <charles.cailleteau@gmail.com>
2024-05-16 17:02:47 +0200

    postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,15 +3,15 @@ const autoPrefixer = require('autoprefixer');
 
 const config = {
 	plugins: [
-		postcssPresetEnv({
-			stage: 3,
-			features: {
-				'nesting-rules': true,
-				'custom-media-queries': true,
-				'media-query-ranges': true
-			}
-		}),
-		autoPrefixer()
+		// postcssPresetEnv({
+		// 	stage: 3,
+		// 	features: {
+		// 		'nesting-rules': true,
+		// 		'custom-media-queries': true,
+		// 		'media-query-ranges': true
+		// 	}
+		// }),
+		// autoPrefixer()
 	]
 };
 
